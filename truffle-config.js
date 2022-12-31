2d88e54203016e71a9e8fbf78f57409bd577e306
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2022-12-31 00:10:04 -0500

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -20,7 +20,8 @@ module.exports = {
     altcoinchain: {
       host: "127.0.0.1",
       port: 8545,
-      network_id: "2330" // Match any network id
+      network_id: "2330", // Match any network id
+      gasPrice: 0
     },
 
 
