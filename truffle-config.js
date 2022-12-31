d563b725ebf3e42e7edf7a4a32d7064cb70a8191
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2022-12-30 23:52:38 -0500

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -4,31 +4,26 @@ const keys = require("./keys.json");
 
 module.exports = {
   contracts_build_directory: "./public/contracts",
+  compilers: {
+    solc: {
+      version: '^0.8.13',
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200,
+        },
+      },
+    },
+  },
+
   networks: {
-    provider: () => 
-        new HDWalletProvider(
-          keys.PRIVATE_KEY,),
     altcoinchain: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "2330" // Match any network id
     },
-    ropsten: {
-      provider: () => 
-        new HDWalletProvider(
-          keys.PRIVATE_KEY,
-          keys.INFURA_ROPSTEN_URL
-        ),
-      network_id: 3,
-      gas: 5500000,
-      gasPrice: 20000000000,
-      confirmations: 2,
-      timeoutBlocks: 200
-    }
+
+
   },
-  compilers: {
-    solc: {
-      version: "0.8.13",
-    }
-  },
+  mocha: {},
 };
